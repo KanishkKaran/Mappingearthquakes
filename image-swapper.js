a62428b12d6323d72651b6723ae5011aa0f1f7fd
Kanishk Karan <35009832+KanishkKaran@users.noreply.github.com>
2018-05-12 00:24:16 -0400

    Update image-swapper.js

--- image-swapper.js
+++ image-swapper.js
@@ -12,34 +12,23 @@ d3.select("#step-two")
   .attr("src", "images/img1.png")
 })
 
-d3.select("#step-three")
-.on('stepin', function(){
-  d3.select("#globe")
-  .attr("src", "images/frame_002_delay-0.06s.gif")
-})
-
 d3.select("#blocks-step-one")
 .on('stepin', function(){
-  d3.select("#blocks")
-  .attr("src", "images/blocks-nothing.gif")
+  d3.select("#mountains")
+  .attr("src", "images/Delhiupshot.jpg")
 })
 
 d3.select("#blocks-step-two")
 .on('stepin', function(){
-  d3.select("#blocks")
-  .attr("src", "images/blocks-red.gif")
+  d3.select("#mountains")
+  .attr("src", "images/RajasthanUpshot.jpg")
 })
 
 d3.select("#blocks-step-three")
 .on('stepin', function(){
-  d3.select("#blocks")
+  d3.select("#mountains")
   .attr("src", "images/blocks-red-yellow.gif")
 })
 
-d3.select("#blocks-step-four")
-.on('stepin', function(){
-  d3.select("#blocks")
-  .attr("src", "images/blocks-all.gif")
-})
 
 })()
